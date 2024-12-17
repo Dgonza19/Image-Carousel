5383c5e1445d6a3a66864643785707c1ca12d1dd
Daisygu19 <daisyg@mikenieva.com>
2024-12-16 22:05:49 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import ImageCarousel from './components/ImageCarousel'
+
+function App() {
+
+  return (
+    <div>
+      <ImageCarousel />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+
+function ImageCarousel() {
+
+    const imagenes = [
+        { src: 'image1.jpg' },
+        { src: 'image2.jpg' },
+        { src: 'image3.jpg' },
+        { src: 'image4.jpg' }
+
+    ]
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+
+    useEffect(() => {
+        if (!isPaused) {
+            const timer = setInterval(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % imagenes.length);
+            }, 4000);
+
+            return () => clearInterval(timer);
+        }
+    }, [isPaused, imagenes.length]);
+
+    useEffect(() => {
+        if (currentIndex >= imagenes.length) {
+            setCurrentIndex(0);
+        }
+    }, [imagenes]);
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? imagenes.length - 1 : prevIndex - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === imagenes.length - 1 ? 0 : prevIndex + 1));
+    }
+
+    return (
+        <div className='carousel'>
+            {
+                imagenes.length > 0 ? (
+                    <div>
+                    <img 
+                        className='carousel-image'
+                        src={imagenes[currentIndex].src} 
+                        alt={`Imagen ${currentIndex + 1}`}
+                    />
+                    <div className='button-container'>
+                    <button onClick={handlePrev} className='carousel-handle-button'>Anterior</button>
+                    <button onClick={() => setIsPaused(true)} className='carousel-paused-button'>Pausar</button>
+                    <button onClick={() => setIsPaused(false)} className='carousel-paused-button'>Eliminar</button>
+                    <button onClick={handleNext} className='carousel-handle-button'>Siguiente</button>
+                    </div>
+                </div>
+                ) : (
+                    <p>No hay imagenes disponibles</p>
+                )
+                
+            }
+        </div>
+    )
+};
+
+export default ImageCarousel;
